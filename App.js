313a68a7ced50e2c0e79151d21bdbcc753c8f115
Alexis Villegas Torres <alexisvt@gmail.com>
2019-05-04 21:21:49 -0600

    add redux to the navigation

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
+/* eslint-disable react/display-name */
+import React from 'react';
 import { Navigation } from 'react-native-navigation';
+import { Provider } from 'react-redux';
 
 import AuthScreen from './src/screens/Auth';
 import FindPlaceScreen from './src/screens/FindPlace';
 import SharedPlaceScreen from './src/screens/SharedPlace';
+import configureStore from './src/store/configureStore';
+
+const store = configureStore();
 
 // Register screens
-Navigation.registerComponent('awesome-places.AuthScreen', () => AuthScreen);
-Navigation.registerComponent('awesome-places.FindPlaceScreen', () => FindPlaceScreen);
-Navigation.registerComponent('awesome-places.SharedPlaceScreen', () => SharedPlaceScreen);
+Navigation.registerComponent(
+  'awesome-places.AuthScreen',
+  () => props => (
+    <Provider store={store}>
+      <AuthScreen {...props} />
+    </Provider>
+  ),
+  () => AuthScreen
+);
+Navigation.registerComponent(
+  'awesome-places.FindPlaceScreen',
+  () => props => (
+    <Provider store={store}>
+      <FindPlaceScreen {...props} />
+    </Provider>
+  ),
+  () => FindPlaceScreen
+);
+Navigation.registerComponent(
+  'awesome-places.SharedPlaceScreen',
+  () => props => (
+    <Provider store={store}>
+      <SharedPlaceScreen {...props} />
+    </Provider>
+  ),
+  () => SharedPlaceScreen
+);
 
 Navigation.events().registerAppLaunchedListener(() => {
   Navigation.setRoot({
