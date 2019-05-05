e9496c68ee264978963b500699e65b1bf1f5204d
Alexis Villegas Torres <alexisvt@gmail.com>
2019-05-04 21:11:17 -0600

    add styles to the tab navigation

--- src/screens/MainTabs/startMainTabs.js
+++ src/screens/MainTabs/startMainTabs.js
@@ -2,7 +2,14 @@ import { Navigation } from 'react-native-navigation';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const startTabs = async () => {
-  const trash = await Ionicons.getImageSource('ios-trash', 20, '#FF0666');
+  const map = await Ionicons.getImageSource('md-map', 30);
+  const share = await Ionicons.getImageSource('ios-share-alt', 30);
+
+  Navigation.setDefaultOptions({
+    bottomTab: {
+      selectedIconColor: 'blue',
+    },
+  });
 
   Navigation.setRoot({
     root: {
@@ -17,7 +24,12 @@ const startTabs = async () => {
                     options: {
                       bottomTab: {
                         text: 'Find Place',
-                        icon: trash,
+                        icon: map,
+                      },
+                      topBar: {
+                        title: {
+                          text: 'Find Places',
+                        },
                       },
                     },
                   },
@@ -26,17 +38,29 @@ const startTabs = async () => {
             },
           },
           {
-            component: {
-              name: 'awesome-places.SharedPlaceScreen',
-              options: {
-                bottomTab: {
-                  text: 'Shared Place',
-                  icon: trash,
+            stack: {
+              children: [
+                {
+                  component: {
+                    name: 'awesome-places.SharedPlaceScreen',
+                    options: {
+                      bottomTab: {
+                        text: 'Shared Place',
+                        icon: share,
+                      },
+                      topBar: {
+                        title: {
+                          text: 'Shared Places',
+                        },
+                      },
+                    },
+                  },
                 },
-              },
+              ],
             },
           },
         ],
+        options: {},
       },
     },
   });
